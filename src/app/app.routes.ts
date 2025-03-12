c6fdd76228d9412da47563f4ee2bb6b0ae9af64e
pongsathonwon <men1234-2008@hotmail.com>
2025-03-12 10:46:09 +0700

    setup route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { ProchurePageComponent } from './pages/prochure-page/prochure-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "prochure",
+        component: ProchurePageComponent
+    },
+    {
+        path: "",
+        pathMatch: "full",
+        component: SearchPageComponent
+    },
+    {
+        path: "**",
+        redirectTo: ""
+    }
+];
---
+++ src/app/pages/prochure-page/prochure-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-prochure-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './prochure-page.component.html',
+  styleUrl: './prochure-page.component.scss'
+})
+export class ProchurePageComponent {
+
+}
