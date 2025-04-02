762abba480a5eaaf530209fd4974ff314745c7f1
pongsathonwon <men1234-2008@hotmail.com>
2025-04-02 14:29:43 +0700

    inject to route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,10 @@ import { promotionResolver } from './resolvers/promotion/promotion.resolver';
 import { marketingResolver } from './resolvers/marketing/marketing.resolver';
 import { ExternalBrochureComponent } from './pages/external-brochure/external-brochure.component';
 import { NotfoundComponent } from './pages/notfound/notfound.component';
+import { BaseBrochureComponent } from './pages/base-brochure/base-brochure.component';
+import { BROCHURE_TOKEN } from './lib';
+import { ProchureService } from './service/prochure/prochure.service';
+import { MarketingService } from './service/marketing/marketing.service';
 
 export const routes: Routes = [
     {
@@ -16,6 +20,22 @@ export const routes: Routes = [
         path: "marketing/:promoType/:isBkk/:isNew/:wholeType/:token/:idPromotion",
         component: ExternalBrochureComponent,
         resolve: { itemList: marketingResolver },
+    },
+    {
+        path: "v2/prochure/:wholeCode/:promoType",
+        component: BaseBrochureComponent,
+        resolve: { itemList: promotionResolver },
+        providers: [
+            { provide: BROCHURE_TOKEN, useExisting: ProchureService }
+        ]
+    },
+    {
+        path: "v2/marketing/:promoType/:isBkk/:isNew/:wholeType/:token/:idPromotion",
+        component: BaseBrochureComponent,
+        resolve: { itemList: marketingResolver },
+        providers: [
+            { provide: BROCHURE_TOKEN, useExisting: MarketingService }
+        ]
     },
     {
         path: "notfound",
