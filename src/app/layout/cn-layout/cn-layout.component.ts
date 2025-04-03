d053ded0ce04d1e17c755869daef696ff99e548c
pongsathonwon <men1234-2008@hotmail.com>
2025-04-03 22:02:19 +0700

    cn layout

---
+++ src/app/layout/cn-layout/cn-layout.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-cn-layout',
+  imports: [RouterOutlet],
+  templateUrl: './cn-layout.component.html',
+  styleUrl: './cn-layout.component.scss'
+})
+export class CnLayoutComponent {
+
+}
