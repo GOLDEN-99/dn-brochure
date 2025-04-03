ca9b365e349e36574b29c9f12b6c6a370b1ad509
pongsathonwon <men1234-2008@hotmail.com>
2025-04-03 22:00:55 +0700

    .

--- src/app/service/api/api.service.ts
+++ src/app/service/api/api.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { TObj } from '../../types';
+import { catchError, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,6 +12,9 @@ export class ApiService {
   private https = inject(HttpClient)
 
   get<T>(path: string, opt: TObj = {}) {
-    return this.https.get<T>(path, opt)
+    return this.https.get<T>(path, opt).pipe(catchError(err => throwError(() => err)))
+  }
+  post<T>(path: string, body: any, opt: TObj = {}) {
+    return this.https.post<T>(path, body, opt).pipe(catchError(err => throwError(() => err)))
   }
 }
