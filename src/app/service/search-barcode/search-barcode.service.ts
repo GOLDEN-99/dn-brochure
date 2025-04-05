d0695946514a042ec93b16b225d725a8ecdd246d
pongsathonwon <men1234-2008@hotmail.com>
2025-04-05 17:29:59 +0700

    searchBarcode Service

---
+++ src/app/service/search-barcode/search-barcode.service.ts
@@ -0,0 +1,48 @@
+import { effect, inject, Injectable, model, signal } from '@angular/core';
+
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+
+import { catchError, debounceTime, distinctUntilChanged, EMPTY, filter, map, of, switchMap, tap } from 'rxjs';
+import { environment } from '../../../environments/environment';
+import { ApiService } from '../api/api.service';
+import { TAppGoodItem, TGoodItem } from '../../types/cn.type';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SearchBarcodeService {
+
+  constructor() {
+    this.search$.pipe(switchMap(this.fetch)).subscribe({
+      next: data => this.products.update(prev => data ? [data] : []),
+      error: (err) => console.log(err)
+    })
+
+    const pro = effect(() => console.log(this.products()))
+  }
+
+  private url = `${environment.cnPath}/GetBarCode`
+
+  search = signal("")
+
+  onSearch(term: string) {
+    this.search.update(() => term)
+  }
+
+  private search$ = toObservable(this.search).pipe(distinctUntilChanged(), debounceTime(300))
+
+  private api = inject(ApiService)
+
+  private fetch = (term: string) => this.api.get<TGoodItem>(`${this.url}/${term}`)
+    .pipe(
+      map(this.mapCheck),
+      catchError(err => { console.log(err); return of(null) })
+    )
+
+  private mapCheck = ({ lot, ...res }: TGoodItem): TAppGoodItem =>
+    ({ ...res, check: false, subTotal: 0, lot: lot.map(l => ({ ...l, check: false, })) })
+
+  products = signal<TAppGoodItem[]>([])
+
+}
--- src/app/types/cn.type.ts
+++ src/app/types/cn.type.ts
@@ -59,18 +59,74 @@ export interface ICnForm {
 
 export type TCNQueryParams = {
     saleCode: string
-    wholeCode: string
-    wholeNumb: string
+    wholeCode: string //"2981",
+    wholeNumb: string // "25035799",
 }
 
-export type TWholeItem = {
-    wholeCode: string //"2981",
+type TBaseWhole = {
     wholeName: string // "ร้านยาวีแคร์ จ.นครปฐม",
     bankNumb: string // "8830398004",
     bankCode: string // "3",
     bankAcName: string // " คุณธัญภรณ์ กิจทวี ",
+}
+
+export type TWholeItem = {
     code: string // "KTB",
     name: string // "กรุงไทย",
-    wholeNumb: string // "25035799",
     wholeDate: string // "03/12/2025 00:00:00"
+} & TBaseWhole & Omit<TCNQueryParams, 'saleCode'>
+
+export type TReamrk = {
+    id: string //1
+    remark: string
 }
+
+export type TLotItem = {
+    goodCode: string //"13628",
+    lotNumber: TMaybe<string>  //null,
+    expiDate: string // "0001-01-01T00:00:00",
+    goodAmou: number //
+}
+
+export type TGoodItem = {
+    goodCode: string // "9060",
+    goodName: string // "GPO Sidegra 50mg 1x4tab (ควบคุม)",
+    barCode: string // "111235",
+    unitPrice: number,
+    unitCode: string // "6",
+    unitDesc: string // "กล่อง",
+    subTotal: number // 192,
+    lot: TLotItem[]
+}
+
+export type TAppLot = { check: boolean } & TLotItem
+
+export type TAppGoodItem = { check: boolean, lot: TAppLot[] } & Omit<TGoodItem, 'lot'>
+
+export type TOrderRes = {
+    wholeNumb: string // "25035799",
+    orderNumb: string // "WO1825516",
+    wholeCode: string // "2981",
+    goodList: TGoodItem[]
+}
+
+export type TGoodItemReq = {
+    goodcode: string // "string",
+    lotNumber: string
+    goodAmou: number
+    unitcode: string
+    unitprice: number
+    subtotal: number
+    expiDate: string // "2025-04-04T08:09:04.257Z"
+}
+
+export type TCreateReq = {
+    cusStat: string
+    bankcode: string | number // 0, === bankCode
+    motive: string // TReamrk.remark
+    motiveId: string | number // TRemark.id
+    remark: string // note ,
+    totalprice: number
+    image: string[] // [url1, url2, ...]
+    goodList: TGoodItemReq[]
+} & TCNQueryParams & Pick<TBaseWhole, 'bankAcName' | 'bankNumb'>
