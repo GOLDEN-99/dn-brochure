e70382909ceba01f56e43f84c3effb3332c0e72f
pongsathonwon <men1234-2008@hotmail.com>
2025-04-05 17:43:23 +0700

    update upload service

--- src/app/service/cn-upload-image/upload-image.service.ts
+++ src/app/service/cn-upload-image/upload-image.service.ts
@@ -2,6 +2,7 @@ import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { ApiService } from '../api/api.service';
 import { catchError, EMPTY, from, mergeMap, Subject, tap, toArray } from 'rxjs';
+import { TMaybe } from '../../types';
 
 @Injectable({
   providedIn: 'root'
@@ -13,22 +14,32 @@ export class UploadImageService {
   private url = `${environment.cnPath}/GenerateImage`
   private api = inject(ApiService)
 
-  image = signal<string[]>([])
+  body = signal<TBody[]>([])
 
-  body = signal<File[]>([])
-
-  uploadCall = (req: TUpload) => this.api.post<string>(this.url, req)
+  uploadCall = (req: TUpload, index: number) => this.api.post<string>(this.url, req)
     .pipe(
-      tap((res) => this.image.update(prev => [...prev, res])),
+      tap((res) => this.body.update((prev) => prev.map(
+        (body, idx) => idx === index
+          ? ({ img: body.img, path: res })
+          : body
+      ))),
       catchError(err => EMPTY)
     )
 
-  upload = ({ wholeNumb, passWord }: Omit<TUpload, 'img'>) => from(this.body())
-    .pipe(
-      mergeMap((f) => this.uploadCall({ wholeNumb, passWord, img: f }), 1),
-      toArray()
-    )
+  upload = ({ wholeNumb, passWord }: Omit<TUpload, 'img'>) => {
+    const reqList = this.body()
+    return from(reqList)
+      .pipe(
+        mergeMap(({ img }, idx) => this.uploadCall({ wholeNumb, passWord, img }, idx), 1),
+        toArray()
+      )
+  }
 
+}
+
+type TBody = {
+  img: string | File
+  path: TMaybe<string>
 }
 
 type TUpload = { wholeNumb: string; img: string | File, passWord: string }
