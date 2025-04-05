30026f24ddfb8673ff04321ab6e37ad1a632a77d
pongsathonwon <men1234-2008@hotmail.com>
2025-04-05 19:52:58 +0700

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ToastComponent } from "./components/toast/toast.component";
+import { LoadingComponent } from "./components/loading/loading.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, ToastComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.scss'
+  selector: 'app-root',
+  imports: [RouterOutlet, ToastComponent, LoadingComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'dn-prochure';
