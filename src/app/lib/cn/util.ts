ffe8867d9fb9c1cef6c567a408097777632c8b5c
pongsathonwon <men1234-2008@hotmail.com>
2025-04-07 14:26:19 +0700

    add base class

--- src/app/lib/cn/util.ts
+++
@@ -1,4 +0,0 @@
-import { TAppGoodItem, TGoodItem } from "../../types/cn.type";
-
-export const mapCheckCn = ({ lot, ...res }: TGoodItem): TAppGoodItem =>
-    ({ ...res, check: false, lot: lot.map(l => ({ ...l, check: false })) })
