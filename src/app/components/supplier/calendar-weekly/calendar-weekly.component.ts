2542b0d7aa9f3967b8837e5f4b8e28a079922bb1
pongsathonwon <men1234-2008@hotmail.com>
2025-05-09 13:06:28 +0700

    add search supplier component

---
+++ src/app/components/supplier/calendar-weekly/calendar-weekly.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-weekly',
+  imports: [],
+  templateUrl: './calendar-weekly.component.html',
+  styleUrl: './calendar-weekly.component.scss'
+})
+export class CalendarWeeklyComponent {
+
+}
--- src/app/pages/supplier-project/in-out-view/in-out-view.component.ts
+++ src/app/pages/supplier-project/in-out-view/in-out-view.component.ts
@@ -1,10 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CalendarMonthlyComponent } from "../../../components/supplier/calendar-monthly/calendar-monthly.component";
+import { CalendarDailyComponent } from "../../../components/supplier/calendar-daily/calendar-daily.component";
+import { CalendarWeeklyComponent } from '../../../components/supplier/calendar-weekly/calendar-weekly.component';
 
 @Component({
   selector: 'app-in-out-view',
-  imports: [FormsModule, CalendarMonthlyComponent],
+  imports: [FormsModule, CalendarMonthlyComponent, CalendarWeeklyComponent, CalendarDailyComponent],
   templateUrl: './in-out-view.component.html',
   styleUrl: './in-out-view.component.scss'
 })
