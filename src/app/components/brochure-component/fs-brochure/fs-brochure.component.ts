e8acaa5420c83dd87302a4c0c7f185fa35cf8cfe
pongsathonwon <men1234-2008@hotmail.com>
2025-05-29 16:11:39 +0700

    minor styling

--- src/app/components/brochure-component/fs-brochure/fs-brochure.component.ts
+++ src/app/components/brochure-component/fs-brochure/fs-brochure.component.ts
@@ -26,7 +26,6 @@ export class FsBrochureComponent {
 
   dayActive = computed(() => this.head().dayActive)
 
-
   mainClass = computed(() => this.isStatic() ? 'flash-sale-bg flash-sale-page main-gap static' : 'flash-sale-bg flash-sale-page container main-gap')
 
   headClass = computed(() => this.isStatic() ? 'head-space static' : 'head-space')
