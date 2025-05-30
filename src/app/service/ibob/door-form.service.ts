b1e2fc9413ee4eacb5a151075afd37e53fb4c2c0
pongsathonwon <men1234-2008@hotmail.com>
2025-05-30 11:33:44 +0700

    .

--- src/app/service/ibob/door-form.service.ts
+++ src/app/service/ibob/door-form.service.ts
@@ -167,7 +167,7 @@ export class DoorFormService {
 
   private formatTime = (t: NgbTimeStruct) => {
     const { hour, minute } = t
-    return `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}:00`
+    return `${String(hour).padStart(2, '0')}:${String(minute).padStart(2, '0')}`
   }
 
   private prepareArray = (d: TDayDetail) => ({ form, to, ...res }: TDuration): TCreateTimeSlot => ({
