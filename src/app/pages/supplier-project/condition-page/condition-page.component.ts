cef46f97496441f69fa5ad44bdc269c08dd85bb2
pongsathonwon <men1234-2008@hotmail.com>
2025-09-12 13:41:40 +0700

    change condition page to signal

--- src/app/pages/supplier-project/condition-page/condition-page.component.ts
+++ src/app/pages/supplier-project/condition-page/condition-page.component.ts
@@ -2,14 +2,14 @@ import { Component, inject, signal } from '@angular/core';
 import { Router, RouterLink } from '@angular/router';
 import { NgbNavModule } from '@ng-bootstrap/ng-bootstrap';
 import { BaseSupplierForm } from '../../../lib/supplier/baseForm';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TReturnForm } from '../../../service/supplier/supplier-from.service';
 import { SupplierApiService } from '../../../service/supplier/supplier-api.service';
 import { ToastService } from '../../../service/toast/toast.service';
 
 @Component({
   selector: 'app-condition-page',
-  imports: [NgbNavModule, RouterLink, ReactiveFormsModule, RouterLink],
+  imports: [NgbNavModule, RouterLink, FormsModule, RouterLink],
   templateUrl: './condition-page.component.html',
   styleUrl: './condition-page.component.scss'
 })
@@ -18,6 +18,28 @@ export class ConditionPageComponent extends BaseSupplierForm {
   active = signal(1)
 
   form = this.formService.form.controls.condi
+  formState = this.formService.formState
+  updator = this.formService.updator
+  noSupReturn() {
+    this.formState.update(prev => ({
+      ...prev,
+      supReturn: false,
+      supFullBox: false,
+      supSameLot: false,
+      supMonthAfterExp: 0,
+      supMonthBeforeExp: 0
+    }))
+  }
+  noStkReturn() {
+    this.formState.update(prev => ({
+      ...prev,
+      stkReturn: false,
+      stkFullBox: false,
+      stkSameLot: false,
+      stkMonthAfterExp: 0,
+      stkMonthBeforeExp: 0
+    }))
+  }
   private ibobApi = inject(SupplierApiService)
   private router = inject(Router)
   navigateComplete() {
