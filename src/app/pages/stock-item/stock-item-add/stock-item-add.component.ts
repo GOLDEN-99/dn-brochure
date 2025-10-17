ce70ee0651b4e7ade53cf5212808843969da7d60
pongsathonwon <men1234-2008@hotmail.com>
2025-10-17 09:17:47 +0700

    .

--- src/app/pages/stock-item/stock-item-add/stock-item-add.component.ts
+++ src/app/pages/stock-item/stock-item-add/stock-item-add.component.ts
@@ -1,11 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
+import { StockItemApiService } from '../../../service/stock-item/stock-item-api.service';
+import { FormsModule } from '@angular/forms';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { TMaybe } from '../../../types';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { filter, switchMap, tap } from 'rxjs';
+
 
 @Component({
   selector: 'app-stock-item-add',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './stock-item-add.component.html',
   styleUrl: './stock-item-add.component.scss'
 })
 export class StockItemAddComponent {
+  private stockItemServ = inject(StockItemApiService)
+  private modalServ = inject(NgbModal)
+  modeList = [{ key: "goodCode", label: 'รหัสสินค้า' }, { key: "goodName", label: "ชื่อสินค้า" }]
+  mode = signal("goodCode")
+  onModeChange = (mode: string) => {
+    this.mode.set(mode);
+    this.stockItemServ.resetSearch();
+  }
+  isCode = signal(() => this.mode() === "goodCode")
 
+  openSearchProductModal = (ref: any) => {
+    this.modalServ.open(ref, { size: 'lg' });
+  }
+  search = this.stockItemServ.searchProductTerm
+  searchName = (goodName: string) => this.search.update(prev => ({ ...prev, goodName }))
+  searchCode = (goodCode: string) => this.search.update(prev => ({ ...prev, goodCode }))
+  productList = this.stockItemServ.searchProductResult
+
+  selectedProduct = signal<TMaybe<TBaseProduct>>(null)
+  private selectedCode = computed(() => this.selectedProduct()?.goodCode ?? "")
+  private selectedCode$ = toObservable(this.selectedCode)
+  private gp$ = this.selectedCode$.pipe(switchMap(c => this.stockItemServ.getGoodGP(c)), tap((res) => res !== null && this.dnCost.set(res.dnCost)))
+  gp = toSignal(this.gp$, { initialValue: null })
+  onAddProduct(product: TBaseProduct) {
+    this.selectedProduct.set(product);
+    this.modalServ.dismissAll();
+    this.stockItemServ.resetSearch();
+  }
+  dnCost = signal(0)
+  newCost = signal(0)
+}
+
+type TBaseProduct = {
+  goodCode: string
+  goodName: string
+  barCode: string
+  goodStat: boolean
 }
--- src/app/service/stock-item/stock-item-api.service.ts
+++ src/app/service/stock-item/stock-item-api.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable, signal } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { toObservable, toSignal } from '@angular/core/rxjs-interop';
-import { BehaviorSubject, debounceTime, distinctUntilChanged, filter, Observable, Subject, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, debounceTime, distinctUntilChanged, filter, Observable, of, Subject, switchMap, tap } from 'rxjs';
 import { ApiService } from '../api/api.service';
 import { catchErrorAndRethrow, getOrElse } from '../../lib/utli';
 import { TStockSetup } from '../../types';
@@ -14,20 +14,25 @@ export class StockItemApiService {
 
   constructor() { }
 
-  private _search_product_url = `${environment.oi}/other-income/product`
+  private _search_product_url = `${environment.oi}/products`
   private _stock_item_url = `${environment.oi}/stock-item`
+
   private api = inject(ApiService)
-  searhProductTerm = signal({ compCode: "", compName: "" })
-  private searchProductTerm$ = toObservable(this.searhProductTerm).pipe(
-    filter(({ compCode, compName }) => compCode !== "" || compName !== ''),
-    distinctUntilChanged((prev, cur) => prev.compCode === cur.compCode && prev.compName === cur.compName),
-    debounceTime(300)
-  )
-  private searchProduct = (params: { compCode: string; compName: string }) =>
-    this.api.get<TSearchProductResult[]>(`${this._search_product_url}/dn`, { params }).pipe(catchErrorAndRethrow())
+  private _initialSearchValue = { goodCode: "", goodName: "" }
+  searchProductTerm = signal(this._initialSearchValue)
+  resetSearch = () => this.searchProductTerm.set(this._initialSearchValue)
+  private searchProductTerm$ = toObservable(this.searchProductTerm)
+    .pipe(
+      distinctUntilChanged((prev, cur) => prev.goodCode === cur.goodCode && prev.goodName === cur.goodName),
+      debounceTime(300)
+    )
+  private searchProduct = ({ goodCode: barcode, goodName }: { goodCode: string; goodName: string }) => {
+    if (goodName === '' && barcode === '') return of([])
+    return this.api.get<TBaseProduct[]>(`${this._search_product_url}`, { params: { barcode, goodName } }).pipe(catchErrorAndRethrow())
+  }
   private searchResult$ = this.searchProductTerm$.pipe(
     switchMap(t => this.searchProduct(t)),
-    getOrElse<TSearchProductResult[]>([])
+    getOrElse<TBaseProduct[]>([])
   )
   searchProductResult = toSignal(this.searchResult$, { initialValue: [] })
 
@@ -56,4 +61,23 @@ export class StockItemApiService {
       )
   setup = toSignal(this.setup$, { initialValue: [] })
 
+  getGoodGP = (goodCode: string) => {
+    if (goodCode === '') return of(null)
+    return this.api.get<TGPResponse>(`${this._stock_item_url}/good/${goodCode}`).pipe(getOrElse(null))
+  }
+
+}
+
+
+type TBaseProduct = {
+  goodCode: string
+  goodName: string
+  barCode: string
+  goodStat: boolean
+}
+
+type TGPResponse = {
+  goodCode: string
+  dnCost: number
+  priceW3: number
 }
