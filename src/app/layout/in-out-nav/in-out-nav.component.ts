b0a2f4db4c79db758d973107ababd2a93aeedd32
pongsathonwon <men1234-2008@hotmail.com>
2025-10-30 10:41:08 +0700

    (refactor) flaten in-out-component

--- src/app/layout/in-out-nav/in-out-nav.component.ts
+++ src/app/layout/in-out-nav/in-out-nav.component.ts
@@ -5,8 +5,35 @@ import { WarehouseService } from '../../service/ibob/warehouse.service';
 @Component({
   selector: 'app-in-out-nav',
   imports: [RouterOutlet, RouterLink, RouterLinkActive],
-  templateUrl: './in-out-nav.component.html',
-  styleUrl: './in-out-nav.component.scss'
+  template: `
+  <section class="form-wrapper">
+      <div style="display: flex; gap: 16px">
+      @for (warehouse of warehouseList(); track $index) {
+          <a
+            [routerLink]="warehouse.id"
+            queryParamsHandling="preserve"
+            routerLinkActive="active"
+            class="btn btn-outline-success"
+            style="flex: 1 1 auto"
+            role="button"
+            >{{ warehouse.name }}</a
+          >
+      }
+      </div>
+      <router-outlet />
+    </section>
+    <div style="text-align: center; margin: 0; opacity: 0.2">version 1.0</div>
+  `,
+  styles: `
+    .form-wrapper {
+      margin: auto;
+      padding: 1rem;
+      width: 100%;
+      @media (min-width: 992px) {
+        width: 920px;
+      }
+    }
+  `
 })
 export class InOutNavComponent {
   private warehouseServ = inject(WarehouseService)
