74e256fc69849a0377d2cb3d1d8d22bb9527936b
pongsathonwon <men1234-2008@hotmail.com>
2025-11-03 12:33:25 +0700

    (fix) calculation of monthly income not light with step and semi-step

--- src/app/components/other-income/edit/other-income-monthly-edit/lib.ts
+++ src/app/components/other-income/edit/other-income-monthly-edit/lib.ts
@@ -13,58 +13,35 @@ export const calStep: TCalFn = (steps: TOIStepItem[]) => (capAmount: number | nu
     const rawTarget = accAmount + current
     const validAmount = applyCap(rawTarget, capAmount)
     if (validAmount === 0) return 0
-    const cal = steps.reduce(
-        ([remain, result], { min, max, rate }) => {
-            // end of recursive acc === target
-            if (remain === 0) return [0, result]
-            if (max === null) {
-                // no upper bound => last step
-                const cur = remain * rate
-                return [0, result + cur]
-            }
-            const range = max - min
-            if (remain <= range) {
-                // within bracket => last step
-                const cur = remain * rate
-                return [0, result + cur]
-            }
-            //greater than bracket > cal this step and subtract bracket size
-            const cur = range * rate
-            return [remain - range, result + cur]
-        }, [validAmount, 0]
-    )
-    const calIncome = cal[1] / 100
-    return calIncome - accIncome
+    let rawIncome = 0
+    for (const step of steps) {
+        const { min, max, rate } = step
+        const thisStep = validAmount - min
+        if (thisStep <= 0) break
+        if (max === null) {
+            rawIncome += thisStep * rate
+        } else {
+            const amount = Math.min(thisStep, max)
+            rawIncome += amount * rate
+        }
+    }
+    const result = (rawIncome / 100) - accIncome
+    return result >= 0 ? result : 0
 }
 
 export const calSemi: TCalFn = (steps: TOIStepItem[]) => (capAmount: number | null, accAmount: number, accIncome: number,) => (current: number) => {
     const rawTarget = accAmount + current
     const validAmount = applyCap(rawTarget, capAmount)
     if (validAmount === 0) return 0
-    const cal = steps.reduce(
-        (acc, { min, max, rate }) => {
-            const [start, end, _] = acc
-            if (start <= end) {
-                // start <= end >> no further calculation
-                return acc
-            }
-            if (max === null) {
-                // last step break here
-                const cur = start * rate
-                return [start, start, cur]
-            }
-            const range = max - min
-            const diff = start - end
-            if (diff > range) {
-                return [start, end + range, 0]
-            }
-            const cur = start * rate
-            return [start, end, cur]
-
-        }, [validAmount, 0, 0]
-    )
-    const calIncome = cal[1] / 100
-    return calIncome - accIncome
+    let rawIncome = 0
+    for (const step of steps) {
+        const { min, max, rate } = step
+        if (validAmount > min && (max === null || validAmount < max)) {
+            rawIncome += validAmount * rate
+        }
+    }
+    const result = (rawIncome / 100) - accIncome
+    return result >= 0 ? result : 0
 }
 
 const applyCap = (amount: number, capAmount: number | null): number => {
