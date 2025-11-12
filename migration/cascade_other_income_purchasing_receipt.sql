75eb5ca261116642d2a61c5834c69a29181538d7
pongsathonwon <men1234-2008@hotmail.com>
2025-11-12 09:10:57 +0700

    .

--- migration/cascade_other_income_purchasing_receipt.sql
+++ migration/cascade_other_income_purchasing_receipt.sql
@@ -1,7 +1,6 @@
 ALTER TABLE other_income_purchase_receipts
 DROP CONSTRAINT FK_other_income_purchase_receipts_other_income_lists;
 
--- Step 2: Recreate the foreign key constraint with CASCADE DELETE
 ALTER TABLE other_income_purchase_receipts
 ADD CONSTRAINT FK_other_income_purchase_receipts_other_income_lists
 FOREIGN KEY (month_id) 
