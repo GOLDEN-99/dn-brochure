f030f35f03938360ebe65415fcf41dfbd96cc543
pongsathonwon <men1234-2008@hotmail.com>
2025-12-10 12:06:53 +0700

    add detail supplier report to account

--- src/app/pages/other-income/other-income-report/other-income-report.component.ts
+++ src/app/pages/other-income/other-income-report/other-income-report.component.ts
@@ -185,4 +185,25 @@ export class OtherIncomeReportComponent {
         }
       })
   }
+
+  exportSupplierMonthDetial(compType: number) {
+    const compCode = this.compCode()
+    const { month, year } = this.date()
+    this.loading.startLoad()
+    this.reportServ.exportSupplierMonthDetialReport(compType, compCode, { year, month, day: 1 }).subscribe({
+      next: async (res) => {
+        if (res.length === 0) {
+          this.toast.danger('ไม่มีข้อมูล')
+          this.loading.endLoad()
+          return
+        }
+        await this.reportServ.exportManySheet(res,)
+        this.toast.success('สำเร็จ')
+        this.loading.endLoad()
+      }, error: (err) => {
+        this.toast.danger(err);
+        this.loading.endLoad();
+      }
+    })
+  }
 }
