73974887eedcd5bc2cd6e59e3743fb54797b7981
pongsathonwon <men1234-2008@hotmail.com>
2026-05-04 16:39:58 +0700

    [fix] [promotion] [create] - signal base datepicker - binding to ui

--- src/app/components/crm-promotion/create-promotion-subform/promotion-master/promotion-master.component.ts
+++ src/app/components/crm-promotion/create-promotion-subform/promotion-master/promotion-master.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { FieldTree, FormField } from '@angular/forms/signals';
 import { TPromotionMaster } from '../../../../types/crm-promotion.type';
+import { SignalDatepickerComponent } from '../../signal-datepicker.component';
 
 @Component({
   selector: 'app-promotion-master',
-  imports: [FormField],
+  imports: [FormField, SignalDatepickerComponent],
   templateUrl: './promotion-master.component.html',
   styles: '',
 })
---
+++ src/app/components/crm-promotion/signal-datepicker.component.ts
@@ -0,0 +1,106 @@
+import {
+  Component,
+  computed,
+  ElementRef,
+  inject,
+  input,
+  InputSignal,
+  InputSignalWithTransform,
+  model,
+  ModelSignal,
+  OutputRef,
+  viewChild,
+} from '@angular/core';
+import {
+  DisabledReason,
+  FormValueControl,
+  ValidationError,
+  WithOptionalFieldTree,
+} from '@angular/forms/signals';
+import {
+  NgbCalendar,
+  NgbDate,
+  NgbDatepickerModule,
+  NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap';
+import { TMaybe } from '../../types';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-signal-datepicker',
+  imports: [FormsModule, NgbDatepickerModule],
+  template: `
+    <div class="input-group">
+      <div class="dp-hidden position-absolute">
+        <input
+          name="datepicker"
+          ngbDatepicker
+          #cal="ngbDatepicker"
+          tabindex="-1"
+          [ngModel]="value()"
+          (ngModelChange)="onClick($event)"
+          style="border: none"
+        />
+      </div>
+      <input
+        #dpFromDate
+        [id]="id()"
+        class="form-control"
+        [value]="displayDate()"
+        name="dpFromDate"
+        readonly
+        [required]="required()"
+      />
+      <button
+        class="btn btn-outline-secondary bi bi-calendar3"
+        (click)="cal.toggle()"
+        [disabled]="disableClick()"
+        type="button"
+      ></button>
+    </div>
+  `,
+  styles: '',
+})
+export class SignalDatepickerComponent implements FormValueControl<NgbDateStruct> {
+  private readonly calService = inject(NgbCalendar);
+  private readonly inputEl = viewChild<ElementRef<HTMLInputElement>>('dpFromDate');
+  id = input('');
+  value: ModelSignal<NgbDateStruct> = model<NgbDateStruct>(this.calService.getToday());
+  checked?: undefined;
+  errors?:
+    | InputSignal<readonly ValidationError.WithOptionalFieldTree[]>
+    | InputSignalWithTransform<readonly ValidationError.WithOptionalFieldTree[], unknown>
+    | undefined;
+  disabled?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  disabledReasons?:
+    | InputSignal<readonly WithOptionalFieldTree<DisabledReason>[]>
+    | InputSignalWithTransform<readonly WithOptionalFieldTree<DisabledReason>[], unknown>
+    | undefined;
+  readonly?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  hidden?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  invalid?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  pending?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  touched?:
+    | InputSignal<boolean>
+    | ModelSignal<boolean>
+    | OutputRef<boolean>
+    | InputSignalWithTransform<boolean, unknown>
+    | undefined;
+  dirty?: InputSignal<boolean> | InputSignalWithTransform<boolean, unknown> | undefined;
+  name?: InputSignal<string> | InputSignalWithTransform<string, unknown> | undefined;
+  required = input(false);
+  min?: InputSignal<number | undefined> | InputSignalWithTransform<number | undefined, unknown> | undefined;
+  max?: InputSignal<number | undefined> | InputSignalWithTransform<number | undefined, unknown> | undefined;
+  disableClick = input(false);
+  focus(options?: FocusOptions): void {
+    this.inputEl()?.nativeElement.focus(options);
+  }
+  thaiDate(date: NgbDateStruct | null) {
+    if (!date) return 'กรุณาเลือกวันที่';
+    return `${date.day}/${date.month}/${date.year}`;
+  }
+  displayDate = computed(() => this.thaiDate(this.value()));
+  onClick(date: NgbDate) {
+    this.value.set(date);
+  }
+}
