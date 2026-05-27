abfd412010220c72b591186795078b644581ddee
pongsathonwon <men1234-2008@hotmail.com>
2026-05-27 19:43:26 +0700

    [feat] update append free product to contain good code

--- src/app/other-income/purchase/services/other-income-purchase-period.service.ts
+++ src/app/other-income/purchase/services/other-income-purchase-period.service.ts
@@ -18,8 +18,8 @@ export class OtherIncomePurchasePeriodService {
     return this.api.post<any>(`${this.url}/period/${periodId}/order`, { poList: req })
   }
 
-  insertFreeItem(periodId: number, req: TOrderList[]) {
-    return this.api.post<any>(`${this.url}/period/${periodId}/free-item`, { poList: req })
+  insertFreeItem(periodId: number, req: TFreeItemReq) {
+    return this.api.post<{ id: number }>(`${this.url}/period/${periodId}/free-item`, req)
   }
 
   deleteFreeItem(periodId: number, freeItemId: number) {
@@ -57,6 +57,14 @@ type TCreatPeriodReq = {
 type TOrderList = {
   orderNumb: string
   receNumb: string
+  actualAmount: number
+  remark: string
+}
+
+type TFreeItemReq = {
+  orderNumb: string
+  receNumb: string
+  goodCode: string
   actualAmount: number
   remark: string
 }
