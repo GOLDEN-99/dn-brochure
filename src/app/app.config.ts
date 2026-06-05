a2f385b9e000ac2eaa399ccfe551e68d184d3cd1
pongsathonwon <men1234-2008@hotmail.com>
2026-06-05 14:50:45 +0700

    [fix] add global error handler for logging and reload

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,23 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, ErrorHandler, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
 import { provideSignalFormsConfig } from '@angular/forms/signals';
 
+class CustomGlobalErrorHandler implements ErrorHandler {
+  handleError(error: any): void {
+    console.warn(error.message)
+    const chunkFailedMessage = /Loading chunk [\d]+ failed/;
+    if (chunkFailedMessage.test(error.message)) {
+      globalThis.window.location.reload();
+    }
+  }
+
+}
+
 export const appConfig: ApplicationConfig = {
   providers: [
+    { provide: ErrorHandler, useClass: CustomGlobalErrorHandler },
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideHttpClient(),
     provideRouter(routes, withComponentInputBinding()),
